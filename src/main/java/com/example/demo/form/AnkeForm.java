5c5f5106d665f26341d6be6a7cd5ce8a87baf4d1
michi <hira-129no@outlook.jp>
2023-06-21 16:01:22 +0900

    AnkeForm.javaの作成

---
+++ src/main/java/com/example/demo/form/AnkeForm.java
@@ -0,0 +1,43 @@
+package com.example.demo.form;
+
+import com.example.demo.Entity.AnkeEntity;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+/**
+	 * アンケート情報を入力用のFormクラス
+	 * @author 道田
+	 * @version 1.0
+	 */
+	
+@Data
+public class AnkeForm {
+	
+	
+	@NotBlank(message = "店舗名をご記入ください")
+	private String shopName;
+	
+	
+	@NotNull(message = "評価点数をご記入ください")
+	@Min(value = 1, message = "1～5の内から選択してください")
+	@Max(value = 5, message = "1～5の内から選択してください")
+	private Integer evaCd;
+	
+	
+	@NotBlank(message = "理由をご記入ください")
+	private String reason;
+	
+	
+	public AnkeEntity getEntity() {
+		AnkeEntity ankeEntity = new AnkeEntity();
+		ankeEntity.setShopName(shopName);
+		ankeEntity.setEvaCd(evaCd);
+		ankeEntity.setReason(reason);
+		return ankeEntity;
+		
+	}
+}
