a9f8c551fbbaf35ce7ff7301c4437e7530fdd615
igarashi <kariyou12@outlook.jp>
2023-06-23 10:51:37 +0900

    HTMLの追加/controller一部編集

--- src/main/java/com/example/demo/controller/AceController.java
+++ src/main/java/com/example/demo/controller/AceController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,7 +24,7 @@ public class AceController {
 	
 	//ace.htmlからanke.htmlに遷移させる
 	
-	@PostMapping("/anke")
+	@GetMapping("/anke")
 	public ModelAndView sample(@ModelAttribute ModelAndView mv) {
 		mv.setViewName("anke");
 		return mv;
