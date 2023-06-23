53e089cb9e6d9b6fb0ea3f865f7e3575c3ffa823
michi <hira-129no@outlook.jp>
2023-06-23 15:42:02 +0900

    SaleEntityとGenreEntityの変更

--- src/main/java/com/example/demo/entity/GenreEntity.java
+++ src/main/java/com/example/demo/entity/GenreEntity.java
@@ -2,6 +2,8 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -18,6 +20,7 @@ import lombok.Data;
 public class GenreEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "genre_cd")
 	private Integer genreCd;
 
--- src/main/java/com/example/demo/entity/SaleEntity.java
+++ src/main/java/com/example/demo/entity/SaleEntity.java
@@ -4,6 +4,8 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -20,6 +22,7 @@ import lombok.Data;
 public class SaleEntity {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "discount_cd")
 	private Integer discountCd;
 
