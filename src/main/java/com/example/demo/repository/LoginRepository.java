21502819448da062ed084161ed04109b86cc0cec
MaikaNakayama_try@outlook.jp <MaikaNakayama_try@outlook.jp>
2023-06-26 15:12:35 +0900

    LoginRepositoryの修正

--- src/main/java/com/example/demo/repository/LoginRepository.java
+++ src/main/java/com/example/demo/repository/LoginRepository.java
@@ -1,12 +1,6 @@
 package com.example.demo.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import com.example.demo.entity.LoginEntity;
 
 /**
  * 従業員用ログイン画面に入力された情報をDBと照合するためのインターフェース
@@ -16,9 +10,5 @@ import com.example.demo.entity.LoginEntity;
 
 
 public interface LoginRepository extends JpaRepository<LoginRepository, Integer> {
-
-	@Query(value = "SELECT emp_id, question FROM t_employee WHERE emp_id=:emp_id AND question=:question",
-			nativeQuery = true)
-	List<LoginEntity> findByEmpId(@Param("empId") Integer empId);
 
 }
