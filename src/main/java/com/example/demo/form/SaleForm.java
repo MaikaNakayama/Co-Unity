2b3f430ad71c2135cf4ec4118c7dcb216e7a7d1a
Hirama <hirama1847@outlook.jp>
2023-06-27 12:33:30 +0900

    saleFormのmax,minの設定

--- src/main/java/com/example/demo/form/SaleForm.java
+++ src/main/java/com/example/demo/form/SaleForm.java
@@ -3,10 +3,12 @@ package com.example.demo.form;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 /**
  * セール情報を格納するためのフォームクラス
  * @author 平松
@@ -22,10 +24,11 @@ public class SaleForm {
 	@Future(message = "明日以降の日付を入力してください。")
 	private LocalDate date;
 
-	@NotBlank(message = "割引率を入力してください。")
-	@Size(min = 1, max = 90, message = "割引率は1~90までの数字を入力してください。")
+	@NotNull(message = "割引率を入力してください。")
+	@Min(value = 1, message = "割引率は1~90までの数字を入力してください。")
+	@Max(value = 90, message = "割引率は1~90までの数字を入力してください。")
 	private Integer saleRate;
-	
+
 	@NotBlank(message = "謳い文句を入力してください。")
 	private String rec;
 
