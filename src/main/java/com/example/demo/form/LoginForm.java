b8cdca174e838bf7e8fae3c4042cd906e69c3269
Hirama <hirama1847@outlook.jp>
2023-06-27 13:05:01 +0900

    正規表現の変更

--- src/main/java/com/example/demo/form/LoginForm.java
+++ src/main/java/com/example/demo/form/LoginForm.java
@@ -13,7 +13,7 @@ import lombok.Data;
 @Data
 public class LoginForm {
 	@NotNull(message = "従業員番号が入力されていません。")
-	@Pattern(regexp = "^[0-9０－９]*${4,10}", message = "従業員番号は桁数に注意して、1~10の数字で入力してください。")
+	@Pattern(regexp = "^[0-9０－９]{4,10}$", message = "従業員番号は桁数に注意して、1~10の数字で入力してください。")
 	private String empId;
 
 	@NotBlank(message = "秘密の質問が入力されていません。")
