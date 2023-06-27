a8fb13ffde247a12497f17511ef8597f7e709e31
aoki <aaookk11@outlook.jp>
2023-06-27 13:15:51 +0900

    コントローラークラスの修正

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -20,71 +20,78 @@ import lombok.AllArgsConstructor;
 @Controller
 public class EmployeeController {
 
-		private final LoginService loginService;
-//		private final SaleRepository saleRepository;//
-		/**
-		 * 従業員番号と秘密の質問をフォームから取得して遷移先を決定する
-		 * @param loginform 従業員番号、秘密の質問の回答
-		 * @param result エラーメッセージ
-		 * @param mv 遷移先の値
-		 * @return menu.html,login.html
-		 */
+	private final LoginService loginService;
 
-		@PostMapping("/menu")
-		public ModelAndView login(@ModelAttribute @Validated LoginForm loginForm, BindingResult result,ModelAndView mv){
-			//サービスクラスのメソッドを呼び出してエラーチェックを行う
-			if(!result.hasErrors()) {
-			//loginService.validQuestion(loginForm, result);
-			LoginEntity loginEntity=new LoginEntity();
-			loginEntity.setEmpId(Integer.parseInt(loginForm.getEmpId()));
-			loginEntity.setQuestion(loginForm.getQuestion());
-			//loginService.isValidEmpId(loginEntity, result);
-			
-				//エラーがない場合.htmlに遷移する。
-				mv.setViewName("menu");
-				return mv;
-				
-			}else {
-				//エラーがある場合ページにとどまる
-				mv.setViewName("login");
-				return mv;
-			}
+	//		private final SaleRepository saleRepository;//
+	/**
+	 * 従業員番号と秘密の質問をフォームから取得して遷移先を決定する
+	 * @param loginform 従業員番号、秘密の質問の回答
+	 * @param result エラーメッセージ
+	 * @param mv 遷移先の値
+	 * @return menu.html,login.html
+	 */
+
+	@PostMapping("/menu")
+	public ModelAndView login(@ModelAttribute @Validated LoginForm loginForm, BindingResult result, ModelAndView mv) {
+
+		if (result.hasErrors()) {
+			//エラーがある場合login.htmlに遷移する。
+			mv.setViewName("login");
+			return mv;
 		}
 
-		
-		/**
-		 * メニュー画面からセール情報を挿入する画面に遷移する
-		 * @return .html
-		 */
+		//サービスクラスのメソッドを呼び出してエラーチェックを行う
+		LoginEntity loginEntity = new LoginEntity();
+		loginEntity.setEmpId(Integer.parseInt(loginForm.getEmpId()));
+		loginEntity.setQuestion(loginForm.getQuestion());
+		loginService.isValidEmpId(loginEntity, result);
+		loginService.validQuestion(loginForm, result);
 
-		@GetMapping("/info")
-		public ModelAndView info(SaleForm saleForm,ModelAndView mv) {
-			mv.addObject("saleForm", saleForm);
+		if (result.hasErrors()) {
+			//エラーがある場合login.htmlに遷移する。
+			mv.setViewName("login");
+			return mv;
+
+		} else {
+			//エラーがない場合menu.htmlに遷移する。
+			mv.setViewName("menu");
+			return mv;
+		}
+	}
+
+	/**
+	 * メニュー画面からセール情報を挿入する画面に遷移する
+	 * @return .html
+	 */
+
+	@GetMapping("/info")
+	public ModelAndView info(SaleForm saleForm, ModelAndView mv) {
+		mv.addObject("saleForm", saleForm);
+		mv.setViewName("saleinput");
+		return mv;
+	}
+
+	@PostMapping("/salecomplete")
+	public ModelAndView insert(@ModelAttribute @Validated SaleForm saleForm, BindingResult result, ModelAndView mv) {
+		//サービスクラスのメソッドを呼び出してエラーチェックを行う
+		//insertService.valid(saleForm, result);
+
+		if (!result.hasErrors()) {
+			//エラーがない場合、DBに情報を登録し、次画面に遷移する
+
+			SaleEntity saleEntity = new SaleEntity();
+			saleEntity.setGenreCd(saleForm.getGenreCd());
+			saleEntity.setDate(saleForm.getDate());
+			saleEntity.setSaleRate(saleForm.getSaleRate());
+			saleEntity.setRec(saleForm.getRec());
+			//saleRepository.saveAndFlush(saleEntity);
+			mv.setViewName("salecomplete");
+			return mv;
+
+		} else {
+			//エラーがある場合ページにとどまる
 			mv.setViewName("saleinput");
 			return mv;
 		}
-		
-		@PostMapping("/salecomplete")
-		public ModelAndView insert(@ModelAttribute @Validated SaleForm saleForm, BindingResult result, ModelAndView mv) {
-			//サービスクラスのメソッドを呼び出してエラーチェックを行う
-			//insertService.valid(saleForm, result);
-			
-			if(!result.hasErrors()) {
-				//エラーがない場合、DBに情報を登録し、次画面に遷移する
-				
-				SaleEntity saleEntity = new SaleEntity();
-				saleEntity.setGenreCd(saleForm.getGenreCd());
-				saleEntity.setDate(saleForm.getDate());
-				saleEntity.setSaleRate(saleForm.getSaleRate());
-				saleEntity.setRec(saleForm.getRec());
-				//saleRepository.saveAndFlush(saleEntity);
-				mv.setViewName("salecomplete");
-				return mv;
-				
-			}else {
-				//エラーがある場合ページにとどまる
-				mv.setViewName("saleinput");
-				return mv;
-			}
-}
+	}
 }
