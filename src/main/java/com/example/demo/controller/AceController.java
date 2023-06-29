8b4ef2db662c815d9385a6a9e6db5f7590cbe12c
Hirama <hirama1847@outlook.jp>
2023-06-29 13:41:37 +0900

    アンケートフォームの変更

--- src/main/java/com/example/demo/controller/AceController.java
+++ src/main/java/com/example/demo/controller/AceController.java
@@ -27,21 +27,20 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 @Controller
 public class AceController {
-	
+
 	private final AnkeRepository ankeRepository;
 	private final AnkeService ankeService;
 	private final GenreRepository genreRepository;
-	
+
 	//ace.htmlからanke.htmlに遷移させる
-	
+
 	@GetMapping("/anke")
 	public ModelAndView sample(AnkeForm ankeForm, ModelAndView mv) {
 		mv.addObject("ankeForm", new AnkeForm());
 		mv.setViewName("anke");
-		
+
 		return mv;
 	}
-	
 
 	/**
 	 * anke.htmlからankeComplete.htmlに遷移させる。
@@ -50,17 +49,16 @@ public class AceController {
 	 * @return 
 	 * @return
 	 */
-	
+
 	//エラーがある場合はanke.htmlにエラーを返す。ない場合はankeComplete.htmlに遷移。
 	@PostMapping("/ankeComplete")
 	public ModelAndView ankeInput(@ModelAttribute @Validated AnkeForm ankeForm, BindingResult result, ModelAndView mv) {
-		ankeForm.setShopName(ankeForm.getShopName().replace(",",""));
-		
-		
+		ankeForm.setShopName(ankeForm.getShopName().replace(",", ""));
+
 		ankeService.validName(ankeForm, result);
 		ankeService.validReason(ankeForm, result);
 		ankeService.validEmp(ankeForm, result);
-		if(!result.hasErrors()) {
+		if (!result.hasErrors()) {
 			AnkeEntity ankeEntity = new AnkeEntity();
 			ankeEntity.setShopName(ankeForm.getShopName());
 			ankeEntity.setEvaCd(ankeForm.getEvaCd());
@@ -70,11 +68,21 @@ public class AceController {
 			ankeRepository.saveAndFlush(ankeEntity);
 			mv.setViewName("ankecomplete");
 			return mv;
-		}else {
+		} else {
+			String genre = ankeForm.getGenre();
+			String[] shopList = null;
+			if (genre.equals("goods")) {
+				shopList = new String[] { "OWNDAYS", "ダイソー" };
+			} else if (genre.equals("gourmet")) {
+				shopList = new String[] { "コメダ珈琲", "イタリアンダイニングDONA", "リンガーハット", "マクドナルド" };
+			} else if (genre.equals("service")) {
+				shopList = new String[] { "ママショップ加納クリーニング", "マジックミシン", "ラフィネ", "イオンカルチャークラブ" };
+			}
+			mv.addObject("shopList", shopList);
 			mv.setViewName("anke");
 			return mv;
 		}
-		
+
 	}
 
 	/**
@@ -83,12 +91,12 @@ public class AceController {
 	 * @param categoryCd
 	 * @return mv
 	 */
-			@GetMapping("/sale")
-			public ModelAndView bargain(@ModelAttribute ModelAndView mv) {
-				List<SaleGenreEntity> saleList = genreRepository.findAll();
-				mv.addObject("saleList",saleList);
-				mv.setViewName("sale");
-				return mv;
-			}
-			
+	@GetMapping("/sale")
+	public ModelAndView bargain(@ModelAttribute ModelAndView mv) {
+		List<SaleGenreEntity> saleList = genreRepository.findAll();
+		mv.addObject("saleList", saleList);
+		mv.setViewName("sale");
+		return mv;
+	}
+
 }
--- src/main/java/com/example/demo/form/AnkeForm.java
+++ src/main/java/com/example/demo/form/AnkeForm.java
@@ -15,33 +15,35 @@ import lombok.Data;
  */
 @Data
 public class AnkeForm {
-	
-	
+
 	@NotBlank(message = "店舗名をご記入ください")
 	private String shopName;
-	
-	
+
 	@NotNull(message = "評価点数をご記入ください")
 	@Min(value = 1, message = "1～5の中から選択してください")
 	@Max(value = 5, message = "1～5の中から選択してください")
 	private Integer evaCd;
-	
+
 	@NotNull(message = "評価点数をご記入ください")
 	@Min(value = 1, message = "1～5の中から選択してください")
 	@Max(value = 5, message = "1～5の中から選択してください")
 	private Integer cevaCd;
-	
+
 	@NotNull(message = "評価点数をご記入ください")
 	@Min(value = 1, message = "1～5の中から選択してください")
 	@Max(value = 5, message = "1～5の中から選択してください")
 	private Integer sevaCd;
-	
-	
+
 	@NotBlank(message = "理由をご記入ください")
 	private String reason;
-	
+
+	@NotBlank(message = "ジャンルを選択してください。")
 	private String genre;
-	
+
+	public String getShopName() {
+		return this.shopName != null ? this.shopName : "";
+	}
+
 	/**
 	 * AnkeEntityに値を格納
 	 * @return AnkeEntity
@@ -50,10 +52,10 @@ public class AnkeForm {
 		AnkeEntity ankeEntity = new AnkeEntity();
 		ankeEntity.setShopName(shopName);
 		ankeEntity.setEvaCd(evaCd);
-		ankeEntity.setEvaCd(cevaCd);
-		ankeEntity.setEvaCd(sevaCd);
+		ankeEntity.setCevaCd(cevaCd);
+		ankeEntity.setSevaCd(sevaCd);
 		ankeEntity.setReason(reason);
 		return ankeEntity;
-		
+
 	}
 }
