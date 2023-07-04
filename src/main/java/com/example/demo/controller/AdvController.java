86fa189980f40407ba904a86e64a3795b245d7cc
Hirama <hirama1847@outlook.jp>
2023-07-04 14:24:10 +0900

    @AllArgsConstructorの設定

--- src/main/java/com/example/demo/controller/AdvController.java
+++ src/main/java/com/example/demo/controller/AdvController.java
@@ -8,26 +8,28 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entity.SaleGenreEntity;
 import com.example.demo.repository.GenreRepository;
 
+import lombok.AllArgsConstructor;
+
 /*
  * サイネージにDBのセールデータを返すコントローラークラス
  * @author 平松
  * @version 1.0
  */
 @RestController
+@AllArgsConstructor
 public class AdvController {
-		SaleGenreEntity saleGenreEntity;
-		GenreRepository genreRepository;
-		
+	private final GenreRepository genreRepository;
+
 	@GetMapping("/adv")
 	public List<SaleGenreEntity> Adv() {
 		//Entitiyの情報を取得し、Listの中へ入れる。
-		List<SaleGenreEntity> advertise=genreRepository.findAll();
+		List<SaleGenreEntity> advertise = genreRepository.findAll();
 		/*advertise.setGenreName("genreName", saleGenreEntity);
 		advertise.setDate("date", saleGenreEntity);
 		advertise.setSaleRate("saleRate",saleGenreEntity);
 		advertise.setRec("rec",saleGenreEntity);*/
 		return advertise;
-		
+
 	}
-	
+
 }
